934c65e50ba3e0a0b6e8691c6b24ea084d47c690
Glib Savchenko <gsavchenko@artenecy.com>
2026-09-10 16:07:43 +0300

    1

--- Formula.py
+++ Formula.py
@@ -1,12 +1,12 @@
 def inputNumber(variableName):
     check = False
     while check != True:
-        inp = input (f"Введите значение {variableName}= ")
+        inp = input(f"Введите значение {variableName}= ")
         try:
             res = float(inp)
             check = True
         except ValueError:
-            print (f"Введённое значение \"{inp}\" не является числом!")
+            print(f"Введённое значение \"{inp}\" не является числом!")
     return res
 
 a = inputNumber("a")
@@ -14,14 +14,14 @@ b = inputNumber("b")
 c = inputNumber("c")
 
 value = round((a + b**3) / (1 + c**2))
-print (f"Ответ {value}")
+print(f"Ответ: {value}")
 
 value = (a + b**3) / (1 + c**2)
-print (f"Ответ {value}")
+print(f"Ответ: {value}")
 
-print (f"Ответ: (a + b^3) / (1 + c^2) = {value}")
+print(f"Ответ: (a + b^3) / (1 + c^2) = {value}")
 
-print (f"Ответ: ({a} + {b}^3) / (1 + {c}^2) = {value}")
+print(f"Ответ: ({a} + {b}^3) / (1 + {c}^2) = {value}")
 
 
 
