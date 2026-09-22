0141bccb5935907d7c5e98bcbe63179fccc0dc1b
Glib Savchenko <gsavchenko@artenecy.com>
2026-09-22 14:06:39 +0300

    1

--- logic_demo.py
+++ logic_demo.py
@@ -12,8 +12,13 @@ def main(_args):
 def menu():
     clear_console()    
     #demo_logic_operators()
-    demo_logic_if()
-    #demo_logic_if_ternary()
+    #demo_logic_if()
+    demo_logic_if_ternary()
+    #demo_logic_if_dynamic_param(1) # Dynamic parameter for IF
+    #demo_logic_if_dynamic_param("a") # Dynamic parameter for IF
+    #demo_logic_switch_case_sycle(4)
+    #demo_logic_switch_case_sycle(_case = 4) # The named parameters with different types, int or str, are supported in Python 3.10 and later.
+    #demo_logic_switch_case_sycle(_case = "d") # The named parameters with different types, int or str, are supported in Python 3.10 and later.
     #test()
 
 def clear_console():
@@ -55,6 +60,37 @@ def demo_logic_if_ternary(): # Ternary operator
     a = 1
     b = 2
     print("A") if a > b else print("B")
+
+def demo_logic_if_dynamic_param(_cond):
+    print("==== Demo of logic if dynamic parameter type ====")
+    print(f"Parameter: {_cond}")
+    if _cond == "a" or _cond == 1:
+        print("A!")
+    elif _cond == "b" or _cond == 2:
+        print("B!")
+    elif _cond == "c" or _cond == 3:
+        print("C!")
+    else:
+        pass # Do nothing, just skip to the next statement
+
+def demo_logic_switch_case_sycle(_case=1): # Dynamic parameter type, can be int or str
+    print("==== Demo switch case ====")
+    print(f"Parameter: {_case}")
+    match _case:
+        case 1 | "a":
+            for i in range(10):
+                print(i) #0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        case 2 | "b":
+            for i in range(5, 10):
+                    print(i) #5, 6, 7, 8, 9
+        case 3 | "c":
+            for i in range(5, 10, 2):
+                print(i) #5, 7, 9
+        case 4 | "d":
+            for ch in "Hello world!":
+                print(ch) #H, e, l, l, o,  , w, o, r, l, d, !
+        case _:
+            print("Default case")
 
 
 
--- syntax-demo.py
+++ syntax-demo.py
@@ -35,11 +35,11 @@ def menu():
     #logic_demo.demo_logic_operators()
     #logic_demo.demo_logic_if()
     logic_demo.demo_logic_if_ternary()
-    #demo_logic_if_2(1) # Dynamic parameter for IF
-    #demo_logic_if_2("a") # Dynamic parameter for IF
-    #demo_logic_sycle(4)
-    #demo_logic_sycle(_case = 4) # The named parameters with different types, int or str, are supported in Python 3.10 and later.
-    #demo_logic_sycle(_case = "d") # The named parameters with different types, int or str, are supported in Python 3.10 and later.
+    #logic_demo.demo_logic_if_dynamic_param(1) # Dynamic parameter for IF
+    #logic_demo.demo_logic_if_dynamic_param("a") # Dynamic parameter for IF
+    #logic_demo.demo_logic_switch_case_sycle(4)
+    #logic_demo.demo_logic_switch_case_sycle(_case = 4) # The named parameters with different types, int or str, are supported in Python 3.10 and later.
+    #logic_demo.demo_logic_switch_case_sycle(_case = "d") # The named parameters with different types, int or str, are supported in Python 3.10 and later.
     #print(demo_function_return(1)) # The returned value can be of any type, int, str, list, dict, etc.
     #print(demo_function_return(2)) # The returned value can be of any type, int, str, list, dict, etc.
     #print(demo_a_la_ax_strfmt("Test: %1, %2!", "Text", 123)) # Dynamic parameters
@@ -201,37 +201,6 @@ def demo_variables_and_inner_functions():
         print("Local inner 2: ", s)
     inner_2()
     print("Top function after inner 2:", s)
-
-def demo_logic_if_dynamic_param(_cond):
-    print("==== Demo of logic if dynamic parameter type ====")
-    print(f"Parameter: {_cond}")
-    if _cond == "a" or _cond == 1:
-        print("A!")
-    elif _cond == "b" or _cond == 2:
-        print("B!")
-    elif _cond == "c" or _cond == 3:
-        print("C!")
-    else:
-        pass # Do nothing, just skip to the next statement
-
-def demo_logic_sycle(_case=1): # Dynamic parameter type, can be int or str
-    print("==== Demo switch case ====")
-    print(f"Parameter: {_case}")
-    match _case:
-        case 1 | "a":
-            for i in range(10):
-                print(i) #0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-        case 2 | "b":
-            for i in range(5, 10):
-                    print(i) #5, 6, 7, 8, 9
-        case 3 | "c":
-            for i in range(5, 10, 2):
-                print(i) #5, 7, 9
-        case 4 | "d":
-            for ch in "Hello world!":
-                print(ch) #H, e, l, l, o,  , w, o, r, l, d, !
-        case _:
-            print("Default case")
 
 def demo_function_return(_case=1):
     print("==== Demo switch case ====")
